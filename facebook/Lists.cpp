8b01c79e2a57d986e03f73b767e0bbe08a8a8d5f
Dan Straussman <danstr@gmail.com>
2017-01-05 16:32:45 +0200

    sssss

--- facebook/Lists.cpp
+++ facebook/Lists.cpp
@@ -60,3 +60,60 @@ int ListFollower::ListSize() const{
 	return currentSize_;
 }
 // **************************  LEADER ********************//
+
+Leader* ListLeader::SetIteratorFirst() {
+	if (firstNode_ == NULL)
+		return NULL;
+	iterator_ = firstNode_;
+	return iterator_->leader_;
+}
+
+Leader* ListLeader::SetIteratorNext() {
+	if (iterator_ == NULL || iterator_->next_ == NULL)
+		return NULL;
+	iterator_ = iterator_->next_;
+	return iterator_->leader_;
+}
+
+Result ListLeader::ListAdd(Leader* leader) {
+	if (leader == NULL)
+		return FAILURE;
+	NodeLeader* newLeaderNode = new NodeLeader;
+	newLeaderNode->leader_ = leader;
+	newLeaderNode->next_ = firstNode_;
+	firstNode_ = newLeaderNode;
+	iterator_ = NULL;
+	currentSize_++;
+	return SUCCESS;
+}
+
+Leader* ListLeader::ReturnIteratorData() const {
+	return iterator_->leader_;
+}
+
+Result ListLeader::ListRemove() { //removes the first element in the list
+	if (currentSize_ > 0 && firstNode_ == iterator_) {
+		NodeLeader* newLeaderNode = iterator_->next_;
+		delete iterator_->leader_;
+		delete iterator_;
+		iterator_ = NULL;
+		firstNode_ = newLeaderNode;
+		currentSize_--;
+		return SUCCESS;
+	}
+
+	NodeLeader* newLeaderNode = firstNode_;
+	while (currentSize_ > 1 && newLeaderNode->next_ != iterator_)
+		newLeaderNode = newLeaderNode->next_;
+	if (newLeaderNode->next_ != NULL)
+		newLeaderNode->next_ = newLeaderNode->next_->next_;
+	delete iterator_->leader_;
+	delete iterator_;
+	iterator_ = NULL;
+	currentSize_--;
+	return SUCCESS;
+}
+
+int ListLeader::ListSize() const {
+	return currentSize_;
+}
